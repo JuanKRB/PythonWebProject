8798828be40437d161945d01e66a2d7d88f0d159
JuanKRB <juanca.rodriguez.bon@gmail.com>
2024-07-24 10:25:02 -0600

    LogIn

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,34 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    if request.method == 'POST':
+        type = request.form.get('type')
+        if type == 'signUp':
+            nameSingUp = request.form.get('nameSingUp')
+            emailSingUp = request.form.get('emailSingUp')
+            password1SingUp = request.form.get('password1SingUp')
+            password2SingUp = request.form.get('password2SingUp')
+
+            if len(emailSingUp) < 4:
+                flash("El email debe ser de 4 caracteres o mas", category='error')
+            elif len(nameSingUp) < 2:
+                flash("El nombre debe ser de 2 caracteres o mas", category='error')
+            elif password1SingUp != password2SingUp:
+                flash("Las contraseñas no son iguales", category='error')
+            elif len(password1SingUp) < 7:
+                flash("La contraseña debe ser de 7 caracteres o mas", category='error')
+            else:
+                flash("Cuenta creada", category='succes')
+        else:
+            pass
+
+
+    data = request.form
+    print(data)
+    return render_template("login.html", user="Juan", boolean = False)
 
 @auth.route('/logout')
 def logout():
